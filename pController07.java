b4f82cefcb168aba4f723c15e3f0d11dd6137a7e
neverGiveUpppppp <95724704+neverGiveUpppppp@users.noreply.github.com>
2022-04-01 17:34:56 +0900

    Add files via upload

---
+++ pController07.java
@@ -0,0 +1,22 @@
+import MVC.model.DAO.pModelDAO07;
+import MVC.view.pView07;
+
+public class pController07 {
+
+//	controller : Model과 View를 연결해주는 다리역할
+//	 넘겨받은 데이터를 가공시키는 역할
+//	어떤 뷰에 출력할지를 결정	
+
+	private pModelDAO07 pmd = new pModelDAO07();
+	private pView07 pv07 = new pView07();
+	
+	
+	public void selectAll() {
+		 
+		
+	}
+		
+		
+	
+		
+}
---
+++ pRun07.java
@@ -0,0 +1,13 @@
+import MVC.view.pView07;
+
+public class pRun07 {
+	public static void main(String[] args) {
+		
+		pView07 pv = new pView07();
+		pv.mainMenu();
+		
+		
+		
+	}
+	
+}
